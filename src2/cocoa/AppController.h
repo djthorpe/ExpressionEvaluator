9b3cd8c8917285984f6d02a7a4dad429eefb21b3
David Thorpe <djt@snowy.local>
2013-02-10 16:02:43 +0000

    reorg

---
+++ src2/cocoa/AppController.h
@@ -0,0 +1,13 @@
+//
+//  AppController.h
+//  ExpressionEvaluator
+//
+//  Created by David Thorpe on 10/02/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject
+
+@end
---
+++ src2/cocoa/AppController.m
@@ -0,0 +1,17 @@
+//
+//  AppController.m
+//  ExpressionEvaluator
+//
+//  Created by David Thorpe on 10/02/2013.
+//
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+-(void)awakeFromNib {
+	NSLog(@"awake from nib");
+}
+
+@end
